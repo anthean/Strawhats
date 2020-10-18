1f02a2380d7d2c468ab45fc13ffeebac7e2335ea
danieljcabrera <51423091+danieljcabrera@users.noreply.github.com>
2020-10-17 22:02:43 -0700

    Initial commit

---
+++ Strawhats/StrawhatsGame.py
@@ -0,0 +1,26 @@
+import pygame
+
+class StrawhatsGame:
+    def __init__(self):
+        self.running = False
+        
+    def run_game(self):
+    #Initializes and runs CovidBlaster through Pygame
+        #Screen initialization
+        pygame.init()
+        screen = pygame.display.set_mode( (800, 600) )
+        
+        running = True
+        
+        #Game loop
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    
+            
+            
+            
+if __name__ == '__main__':
+    covidBlaster = StrawhatsGame()
+    covidBlaster.run_game()

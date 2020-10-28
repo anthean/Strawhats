d5eb48b18ed5399d550d1a15df51ab71eb8f98cf
LeftButtox <alexanvl@uci.edu>
2020-10-28 03:08:23 -0700

    set up a bunch of stuff: - display surface to draw on - set up while loop - tested drawing a rectangle - set up button press method

--- Strawhats/StrawhatsGame.py
+++ Strawhats/StrawhatsGame.py
@@ -1,14 +1,27 @@
 # TODO:
-# add clock
-# add character to screen
-# add surface to draw on
 
-#testting
-#asdfasdf
-
-import pygame
+import pygame, sys
 import mainCharacter
 
+# Game stats
+_FRAME_RATE = 10
+_FRAME_WIDTH = 800
+_FRAME_HEIGHT = 600
+_BACKGROUND_COLOR = pygame.Color(100,100,100)
+
+# Colors
+red = pygame.Color(255,0,0)
+green = pygame.Color(0,255,0)
+blue = pygame.Color(0,0,225)
+teal = pygame.Color(0,255,255)
+yellow = pygame.Color(255,255,0)
+pink = pygame.Color(255,0,255)
+black = pygame.Color(0,0,0)
+
+colors = [red, green, blue, teal, yellow, pink]
+
+
+# Character stats
 _charHealth = 100
 _charXPos = 400
 _charYPos = 300
@@ -16,36 +29,68 @@ _charYPos = 300
 #test
 class StrawhatsGame:
     def __init__(self):
-        self.running = False
+        self.running = True
+        self.pixelWidth = _FRAME_WIDTH
+        self.pixelHeight = _FRAME_HEIGHT
+        #self.surface will be made when create_surface is called
         
     def run_game(self):
-    #Initializes and runs CovidBlaster through Pygame
-        #Screen initialization
+        #Initializes and runs CovidBlaster through Pygame
+        # Pygame initialization
         pygame.init()
-        screen = pygame.display.set_mode( (800, 600) )
-        
-        running = True
 
-        # create game character
-        character = mainCharacter.MainCharacter(_charHealth, _charXPos, _charYPos)
-        character.print_name() #prints chicken to the console
+        try:
+            clock = pygame.time.Clock()
+            self.surface = pygame.display.set_mode((self.pixelWidth, self.pixelHeight))
 
-        #Game loop
-        while running:
-
-            #draw character model here
+            test_surface = pygame.Surface((100,200))
+            test_surface.fill((0,0,100))
 
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
+            # create game character
+            character = mainCharacter.MainCharacter(_charHealth, _charXPos, _charYPos)
+            character.print_name()  # prints chicken to the console
 
-                # if arrow key is hit
-                    # move character up
-                    
-            
-            
-            
+            while self.running:
+                clock.tick(_FRAME_RATE)
+
+                # handle events
+                self.handle_events()
+
+                # draw frames
+                self.draw_frames()
+
+                self.surface.blit(test_surface,(100,100))
+                pygame.display.update()
+
+        finally:
+            pygame.quit()
+            sys.exit()
+
+    def handle_events(self) -> None:
+        """ loops through events from pygame """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("quiting event")
+                self.running = False
+
+        self.handle_keys() # button presses
+
+    def handle_keys(self) -> None:
+        """ Changes objects in the game  """
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_SPACE]:
+            print("shoot")
+            # shoot
+
+    def draw_frames(self):
+        """ called in main method. responsible for refreshing frame after each call """
+        self.surface.fill(_BACKGROUND_COLOR)
+
+
+        pygame.display.update()
+
 if __name__ == '__main__':
     covidBlaster = StrawhatsGame()
     covidBlaster.run_game()

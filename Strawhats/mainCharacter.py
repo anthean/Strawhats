2543064eac96e617a523bfa31aac17cd710a5bd6
danieljcabrera <51423091+danieljcabrera@users.noreply.github.com>
2020-11-03 18:29:08 -0800

    Update mainCharacter.py

--- Strawhats/mainCharacter.py
+++ Strawhats/mainCharacter.py
@@ -1,3 +1,4 @@
+import pygame
 # todo:
 # don't let the character move off screen
 # add health modifier
@@ -9,14 +10,16 @@ class MainCharacter:
         self.x_pos = x
         self.y_pos = y
 
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = pygame.Rect(self.x_pos, self.y_pos, 15, 15)
 
     def print_name(self):
         print(self.name)
 
     def moveVertically(self, distance, highestDistance, smallestDistance):
-
         self.y_pos = self.y_pos + distance
+        self.rect = pygame.Rect(self.x_pos, self.y_pos, 15, 15)
 
     def moveHorizontally(self, distance, highestDistance, smallestDistance):
         self.x_pos = self.x_pos + distance
-
+        self.rect = pygame.Rect(self.x_pos, self.y_pos, 15, 15)             #KIM: what is highestDistance and smallestDistance supposed to be?

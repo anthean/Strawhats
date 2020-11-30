f7503cf66b41da96d4bac6fa7b80fa4b3f4774a2
luffy <trankn4@gmail.com>
2020-11-30 00:05:03 -0800

    update

--- src/gamestate.py
+++ src/gamestate.py
@@ -1,9 +1,12 @@
-from math import sqrt
 import pygame_functions as pgf
+from math import sqrt
 from player import Player
+
 
 class GameState:
     def __init__(self):
+        self.screen_w = None
+        self.screen_h = None
         self.name = ''
         self.player = None
         self.score = 0
@@ -12,14 +15,19 @@ class GameState:
     def start(self, WIDTH, HEIGHT, FULLSCREEN):
         if FULLSCREEN: pgf.screenSize(WIDTH, HEIGHT, fullscreen=True)
         else: pgf.screenSize(WIDTH, HEIGHT)
+        self.screen_w = WIDTH
+        self.screen_h = HEIGHT
         self.score = 0
         self.game_over = False
+        self.load_background()
+        pgf.endWait()
+
+    def load_background(self):
         bg = pgf.makeSprite('./assets/sprites/STAGE/stage.png')
-        scale = int(sqrt(WIDTH*HEIGHT / 36864))
+        scale = int(sqrt(self.screen_w*self.screen_h/36864))
         pgf.transformSprite(bg, scale=scale)
         pgf.moveSprite(bg, 0, 0)
         pgf.showSprite(bg)
-        pgf.endWait()
 
     def set_player(self, pcolor):
         self.player = Player(pcolor)
@@ -30,11 +38,3 @@ class GameState:
     def set_score(self, score):
         self.score = score
 
-    def name(self) -> str:
-        return self.name
-
-    def score(self) -> int:
-        return self.score
-
-    def game_over(self) -> bool:
-        return self.game_over
